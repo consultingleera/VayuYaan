76be9dce05eba9c264136aa29b4a39641cded861
Deeptanshu Sankhwar <deeptanshu@walletsocket.com>
2022-12-02 08:03:22 +0530

    first-commit

---
+++ VayuYaan-Server/src/routes/pilot.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const pilotController = require('../controllers/pilot');
+
+router.post('/create', pilotController.createPilot);
+router.get('/', pilotController.viewPilots);
+router.get('/:id', pilotController.viewPilot);
+router.delete('/delete/:id', pilotController.deletePilot);
+router.put('/edit/:id', pilotController.editPilot);
+
+module.exports = router;
